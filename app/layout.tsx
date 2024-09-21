d151f4ae3ac65e62b883982f4ac8b78a5e9f7b89
Rafael Kristoforus Yanto <132446500+izruff@users.noreply.github.com>
2024-09-21 15:45:58 +0800

    Revert "Feat/home page (#17)" (#23)
    This reverts commit 86be27f0f8810e0a6cfbbe3010fae78cc5167e03.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import { Suspense } from "react";
 import ModalOverlay from "./components/modal-overlay";
 import Link from "next/link";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <ModalOverlay />
       </body>
     </html>
   );
