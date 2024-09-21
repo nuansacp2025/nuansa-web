6f53f9a7225d0c4ebba115497960b18e3fe1f60f
izruff <rafaelky.dev@gmail.com>
2024-09-21 16:26:06 +0800

    Update navbar links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,13 +7,16 @@ const Navbar = () => {
         className={`fixed inset-0 z-10 bg-gray-800 bg-opacity-75 transition-opacity lg:relative lg:flex lg:gap-x-12 lg:bg-transparent opacity-100`}
       >
         <div className="flex flex-col lg:flex-row lg:gap-x-12 lg:items-center lg:justify-center p-4 lg:p-0">
-          <Link href="/about" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
-            About
+          <Link href="/" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
+            Home
           </Link>
-          <Link href="/gallery" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
-            Gallery
+          <Link href="/about-us" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
+            About Us
           </Link>
-          <Link href="/contact" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
+          <Link href="/our-team" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
+            Our Team
+          </Link>
+          <Link href="/contact-us" className="text-sm font-semibold leading-6 text-white lg:text-gray-0">
             Contact Us
           </Link>
         </div>
