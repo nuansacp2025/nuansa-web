7c552208a29cd103b5857661abf16e2a6119a4b4
izruff <rafaelky.dev@gmail.com>
2024-09-21 11:53:14 +0800

    Remove unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
-import { Suspense } from "react";
 import ModalOverlay from "./components/modal-overlay";
 import Link from "next/link";
 
