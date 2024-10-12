4b2ecc4f7dc8b00a94860644866b9a6d16335529
izruff <rafaelky.dev@gmail.com>
2024-10-12 22:24:19 +0800

    Fix layout and reorganize code

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -22,7 +22,21 @@ export default function AboutUs() {
         
         isModalOpen && 
         <ModalOverlay onClose={onClose}>
-          <div className="text-3xl mb-8 text-lime-900">headddddddddasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffadfasdhe</div>
+          <div className="text-3xl mb-8 text-lime-900">
+            headddddddddasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffadfasdhe
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </ModalOverlay>
         
       }
--- app/components/modal-overlay.tsx
+++ app/components/modal-overlay.tsx
@@ -28,17 +28,17 @@ function ModalOverlay({ onClose, children }: ModalOverlayProps) {
   return (
     <div
       id="modal-overlay"
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+      className="fixed max-h-screen py-10 inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
       onClick={handleOutsideClick}
     >
-      <div className="bg-white rounded-lg shadow-lg p-6 relative mx-auto max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
+      <div className="bg-white rounded-lg shadow-lg p-6 pt-10 relative max-w-md max-h-full sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl overflow-auto">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
         >
           &times;
         </button>
-        <div className="mt-5 overflow-auto">{children}</div>
+        {children}
       </div>
     </div>
   );
