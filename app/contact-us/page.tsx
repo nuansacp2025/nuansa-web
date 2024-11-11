2832c941d0ed10a5a6c188b87c92d6ab77239e83
Rafael Kristoforus Yanto <132446500+izruff@users.noreply.github.com>
2024-11-11 18:22:59 +0800

    Add fade-ins (#54)

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,11 +1,14 @@
+"use client"
+
 import Image from "next/image";
 import BusinessForm from "@/app/components/contact-us/business-form";
 import GeneralForm from "@/app/components/contact-us/general-form";
+import { FadeInDiv } from "../components/animations";
 
 export default function ContactPage() {
     return (
         <div className="items-center bg-green-a space-y-20 px-4 md:px-12 lg:px-16 pb-12">
-            <div className="sm:grid sm:grid-cols-2 text-white py-10">
+            <FadeInDiv className="sm:grid sm:grid-cols-2 text-white py-10">
                 <div className="sm:place-self-center mx-auto my-auto">
                     <GeneralForm/>
                 </div>
@@ -13,8 +16,8 @@ export default function ContactPage() {
                     <Image src={'/images/logo/nuansa_logo.png'} alt={'logo'} width={0} height={0} sizes="100vw"
                            style={{ width: '12rem', height: 'auto' }}/>
                 </div>
-            </div>
-            <div className="sm:grid sm:grid-cols-2 bg-orange-a rounded-md text-black bottom-8 p-10">
+            </FadeInDiv>
+            <FadeInDiv className="sm:grid sm:grid-cols-2 bg-orange-a rounded-md text-black bottom-8 p-10">
                 <div className="sm:place-self-center hidden sm:block">
                     <Image src={'/images/logo/nuansa_logo.png'} alt={'logo'} width={0} height={0} sizes="100vw"
                            style={{width: '12rem', height: 'auto'}}/>
@@ -22,7 +25,7 @@ export default function ContactPage() {
                 <div className="sm:place-self-center">
                     <BusinessForm/>
                 </div>
-            </div>
+            </FadeInDiv>
         </div>
     )
             }
--- app/our-team/arts/page.tsx
+++ app/our-team/arts/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import MemberCard from "@/app/components/our-team/member-card";
 import Image from "next/image";
+import { FadeInDiv } from "@/app/components/animations";
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <section className="relative w-full py-12 px-16 overflow-hidden md:py-20 md:px-0">
         <Image src={"/images/our-team/what-we-do.png"} alt="What We Do" fill={true} objectFit="cover" />
-        <div className="relative w-full flex flex-col items-center md:flex-row">
+        <FadeInDiv className="relative w-full flex flex-col items-center md:flex-row">
           <div className="flex flex-1 items-center justify-center mb-12 md:mb-0">
             <h2 className="text-4xl md:text-5xl font-bold text-white text-center">
               ARTS
@@ -20,23 +23,27 @@ export default function Page() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
-        </div>
+        </FadeInDiv>
       </section>
       
       <section className="w-full flex flex-col items-center py-12 px-16 md:py-20 md:px-32">
-        <h2 className="text-3xl md:text-4xl pb-8 md:pb-12 font-bold text-green-a">
-          Divisions and Tasks
-        </h2>
-        <h3 className="text-2xl md:text-3xl pb-6 md:pb-8 font-semibold text-green-a">
-          Members
-        </h3>
-        <div className="grid gap-8 place-content-center place-items-stretch grid-cols-[minmax(0,_350px)] md:grid-cols-[repeat(2,_minmax(0,_350px))] lg:grid-cols-[repeat(3,_minmax(0,_350px))] md:gap-16">
+        <FadeInDiv>
+          <h2 className="text-3xl md:text-4xl pb-8 md:pb-12 font-bold text-green-a">
+            Divisions and Tasks
+          </h2>
+        </FadeInDiv>
+        <FadeInDiv>
+          <h3 className="text-2xl md:text-3xl pb-6 md:pb-8 font-semibold text-green-a">
+            Members
+          </h3>
+        </FadeInDiv>
+        <FadeInDiv className="grid gap-8 place-content-center place-items-stretch grid-cols-[minmax(0,_350px)] md:grid-cols-[repeat(2,_minmax(0,_350px))] lg:grid-cols-[repeat(3,_minmax(0,_350px))] md:gap-16">
           <MemberCard imageSrc="/images/our-team/member-placeholder.png" name="Member Name 1" role="Role 1" />
           <MemberCard imageSrc="/images/our-team/member-placeholder.png" name="Member Name 2" role="Role 2" />
           <MemberCard imageSrc="/images/our-team/member-placeholder.png" name="Member Name 3" role="Role 3" />
           <MemberCard imageSrc="/images/our-team/member-placeholder.png" name="Member Name 4" role="Role 4" />
           <MemberCard imageSrc="/images/our-team/member-placeholder.png" name="Member Name 5" role="Role 5" />
-        </div>
+        </FadeInDiv>
       </section>
     </main>
   )
