2c9ecba453b50e5085098d84b97bd5c75629567f
FerdiHS <92571864+FerdiHS@users.noreply.github.com>
2025-02-06 00:26:31 +0700

    Initial About Us Keong Mas Page (#72)
    * Create unit tests for About Us Keong Mas Page
    
    * Create initial page for About Us Keong Mas
    
    ---------
    
    Co-authored-by: Rafael Kristoforus Yanto <132446500+izruff@users.noreply.github.com>

---
+++ app/components/about-us/keong-mas-2024/main-casts.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+
+interface CastProps {
+  cast: CastMember[];
+}
+
+const MainCast: React.FC<CastProps> = ({ cast }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      {cast.map((member, index) => (
+        <div
+          key={index}
+          data-testid="cast-card"
+          className="w-full sm:w-80 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-green-a dark:border-gray-700 
+                     transform transition-all duration-300 hover:scale-105 hover:shadow-2xl cursor-pointer"
+        >
+          <div className="relative w-full h-64">
+            <Image
+              src={member.image}
+              alt={member.name}
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-t-lg"
+              priority={index < 3}
+            />
+          </div>
+          <div className="p-5">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {member.name}
+            </h5>
+            <p className="text-gray-700 dark:text-gray-400">
+              <strong>Character:</strong> {member.character}
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {member.description}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MainCast;
---
+++ app/types.tsx
@@ -0,0 +1,12 @@
+interface ImageConfig {
+    src: string;
+    alt: string;
+    description?: string;
+}
+
+interface CastMember {
+    name: string;
+    character: string;
+    image: string;
+    description?: string;
+}
