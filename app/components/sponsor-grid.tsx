fc618f48abbd51af6cd9cdfc80c0cfb8ae50c6af
izruff <rafaelky.dev@gmail.com>
2025-08-04 11:36:33 +0800

    Move code to pass failed tests

--- app/components/sponsor-grid.tsx
+++ app/components/sponsor-grid.tsx
@@ -35,32 +35,6 @@ const SponsorGrid: React.FC<SponsorGridProps> = ({ sponsors }) => {
           ))
         }
       </div>
-      {/* TODO: try to refactor this */}
-      <div className="bg-white bg-opacity-80 text-black rounded-lg mt-8 p-8 space-y-4 flex flex-col">
-        <h3 className="text-green-a text-2xl font-semibold mb-4">Supported By</h3>
-        <div className="flex justify-center transition-transform duration-200 hover:scale-105">
-          <h4 className="text-lg">Stephen Riady Group of Foundations</h4>
-        </div>
-        <div className="flex justify-center transition-transform duration-200 hover:scale-105">
-          <h4 className="text-lg">NUS Office of Student Affairs</h4>
-        </div>
-        <div className="flex justify-center">
-          <img
-            src="/images/supported-by/nussu.png"
-            alt="NUSSU Logo"
-            style={{ maxHeight: "100px" }}
-            className={`transition-transform duration-200 hover:scale-105`}
-          />
-        </div>
-        <div className="flex justify-center">
-          <img
-            src="/images/supported-by/nyc.png"
-            alt="NYC Logo"
-            style={{ maxHeight: "150px" }}
-            className={`transition-transform duration-200 hover:scale-105`}
-          />
-        </div>
-      </div>
     </div>
   );
 };
